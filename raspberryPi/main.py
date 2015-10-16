c9b02d11badff04e9d11b8a18940f1b4b3c9504b
boblecodeur <tristanplouz@gmail.com>
2015-10-16 19:41:49 +0200

    Modification de la forme du JSON et ajout de fonctionnalité La ligne 3 ne GET pas correctement les passages car son arret est "liberté place" et non "liberté"

--- raspberryPi/main.py
+++ raspberryPi/main.py
@@ -1,13 +1,11 @@
 #! /usr/bin/python3
 import bibus
 
-
 import sched, time, json
 from datetime import datetime
 
 class Bibus2Arduino:
-    json_data=open('data/arret.json')
-    data = json.load(json_data)
+
     def __init__(self):
         print("Starting...")
 
@@ -17,10 +15,13 @@ class Bibus2Arduino:
         print("Bibus API used version : 1.1 released the 09/09/2015")
         print("Don't hesitate to open a new issue on https://github.com/mdl29/panobus on any bug")
 
-
+        self.json_data=open('data/arret.json')
+        self.arret = json.load(self.json_data)
         print("Info du fichier json des arrets")
-        for arret in range(len(self.data["arret"])):
-            print(self.data["arret"][arret]["name"]," a pour dest ",self.data["arret"][arret]["dest"][0]," et ", self.data["arret"][arret]["dest"][1])
+        for arret in range(len(self.arret)):
+            for route in range(len(self.arret[arret])):
+                for dest in range(len(self.arret[arret]["route"][route]["dest"])):
+                    print(self.arret[arret]["name"]," a pour destination, sur la ligne ",self.arret[arret]["route"][route]["name"],self.arret[arret]["route"][route]["dest"][dest])
 
         self.s = sched.scheduler(time.time, time.sleep)
         self.s.enter(6, 1, self.loop)
@@ -28,8 +29,10 @@ class Bibus2Arduino:
     
     def getData(self):
         print("get data")
-        for i in range(len(self.data["arret"])):
-            print(self.b.getRemainingTimes(self.data["arret"][i]["route"][0],self.data["arret"][i]["name"],self.data["arret"][i]["dest"][0]))
+        for arret in range(len(self.arret)):
+            for route in range(len(self.arret[arret])):
+                for dest in range(len(self.arret[arret]["route"][route]["dest"])):
+                    print("\n",self.b.getRemainingTimes(self.arret[arret]["route"][route]["name"], self.arret[arret]["name"],self.arret[arret]["route"][route]["dest"][dest]))
 
     def processData(self,data):
         pass
