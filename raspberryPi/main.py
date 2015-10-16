fad7ea636c2fb950ac15442fc7ea99b277be61fc
Naegi <gouge.tristan@gmail.com>
2015-10-16 21:55:43 +0200

    nettoyage du code

--- raspberryPi/main.py
+++ raspberryPi/main.py
@@ -1,51 +1,81 @@
 #! /usr/bin/python3
 import bibus
 
+import sched, time # for a 60sec scheduler
+import json # For the file import
+import logging # For ... logs
 
-import sched, time, json
-from datetime import datetime
+logging.basicConfig(filename='bibus.log', level=logging.DEBUG)
+debug = logging.debug
+info = logging.info
+warning = logging.warning
 
 class Bibus2Arduino:
-    json_data=open('data/arret.json')
-    data = json.load(json_data)
     def __init__(self):
-        print("Starting...")
+        info("Starting...")
+        
+        info("Processing data file...")
+
+        with open('data/arret.json') as json_data:
+            self.data = json.load(json_data)
+        if not self.data:
+            raise Exception("Can't load file !")
 
         self.b = bibus.Bibus()
 
-        print("Bibus API last version :", self.b.getVersion()[0]['Number'],"released the", self.b.getVersion()[0]["Date"])
-        print("Bibus API used version : 1.1 released the 09/09/2015")
-        print("Don't hesitate to open a new issue on https://github.com/mdl29/panobus on any bug")
+        debug("Bibus API last version : {} released the {}".format(self.b.getVersion()[0]['Number'],self.b.getVersion()[0]["Date"]))
+        debug("Bibus API used version : 1.1 released the 09/09/2015")
+        info("Don't hesitate to open a new issue on https://github.com/mdl29/panobus on any bug")
 
 
-        print("Info du fichier json des arrets")
-        for arret in range(len(self.data["arret"])):
-            print(self.data["arret"][arret]["name"]," a pour dest ",self.data["arret"][arret]["dest"][0]," et ", self.data["arret"][arret]["dest"][1])
+        info("Info du fichier json des arrets")
+
+        for arret in self.data:
+            for route in arret["route"]:
+                for dest in route["dest"]:
+                    debug("Reading JSON ... {}({}) -> {}".format(arret["name"],route["name"], dest))
 
         self.s = sched.scheduler(time.time, time.sleep)
-        self.s.enter(6, 1, self.loop)
+        self.s.enter(0, 1, self.loop) # No wait the first time
+ 
         self.s.run()
     
+
     def getData(self):
-        print("get data")
-        for i in range(len(self.data["arret"])):
-            print(self.b.getRemainingTimes(self.data["arret"][i]["route"][0],self.data["arret"][i]["name"],self.data["arret"][i]["dest"][0]))
+        debug("Get data : ")
+        for arret in self.data:
+            for route in arret["route"]:
+                for dest in route["dest"]:
+                    remainingTime = self.b.getRemainingTimes(route["name"],arret["name"], dest)
+                    try:
+                        remainingTimeVal = remainingTime[0][0]['Remaining_time']
+                    except IndexError:
+                        warning('Bad URI ? : {}'.format(remainingTime[1]))
+
+                    debug("Remaining time for {}({}) -> {} : {}".format(arret["name"],route["name"], dest,remainingTimeVal))
 
     def processData(self,data):
         pass
 
+    """
+        See protocole.md for more informations 
+    """
     def sendData(self, processData):
-		#See the file protocole.md for more information
         pass
 
+    """
+        A loop restarting all 60sec which do the whole cycle 
+    """
     def loop(self):
+        #should don't be changed, look to subfunctions instead
         data = self.getData()
         processedData = self.processData(data)
         self.sendData(processedData)
 
-        self.s.enter(6, 1, self.loop) #Wait for one minute
+        self.s.enter(60, 1, self.loop) #Wait for one minute
 
 if __name__=="__main__":
     Bibus2Arduino()
 
-print("Shut down normally...")
+
+info("Shut down normally...")
