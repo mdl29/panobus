5c00e798374738db177b30e4ce81ef24c36597ed
tristanplouz <tristanplouz@gmail.com>
2016-02-09 15:19:25 +0100

    Ajout de leaflet

--- website/js/fullpage.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("#preloader").hide();
-    $('#fullpage').fullpage({
-		anchors: ['pres', 'doc', 'map', 'parten'],
-		sectionsColor: ['#FF0000', '#800080', '#1E90FF', '#FFFF00'],
-		menu: "#menu",
-		css3: true
-	});
-});
---
+++ website/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$("#preloader").hide();
+    $('#fullpage').fullpage({
+		anchors: ['pres', 'doc', 'map', 'parten'],
+		sectionsColor: ['#FF0000', '#800080', '#1E90FF', '#FFFF00'],
+		menu: "#menu",
+		css3: true
+	});
+	var map = L.map('mapLeaflet').setView([48.3921,-4.4769], 13);
+	
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+	
+	L.marker([48.40850,-4.48043]).addTo(map);
+});
