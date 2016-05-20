0da4df48b38f4c25dfd24471a26b36951fa63f28
naegi <gouge.tristan@gmail.com>
2016-05-20 21:22:39 +0200

    clean main.js and fix 10 (to test)

--- website/js/main.js
+++ website/js/main.js
@@ -1,54 +1,32 @@
 $(document).ready(function() {
-	//fullpage
+    $("#preloader").hide();
+
+    //fullpage
     $('#fullpage').fullpage({
-		anchors: ['Panobus','pres', 'news','doc', 'map', 'parten','contacts'],
-		sectionsColor: ['#191919','#352a75', '#423495', '#4f3eb6', '#6857c7','#8477d3','#191919'],
-		menu: "#menu",
-		css3: true,
-		normalScrollElements:"#mapLeaflet",
-		paddingTop:"6%",
-		sectionSelector:"section",
+        anchors: ['Panobus','pres', 'news','doc', 'map', 'parten','contacts'],
+        sectionsColor: ['#191919','#352a75', '#423495', '#4f3eb6', '#6857c7','#8477d3','#191919'],
+        menu: "#menu",
+        normalScrollElements:"#mapLeaflet",
+        paddingTop:"64px",
+        sectionSelector:"section",
+    });
 
-		//Scrolling
-        scrollingSpeed: 700,
-        autoScrolling: true,
-        fitToSectionDelay: 1000,
-        scrollBar: false,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        scrollOverflow: false,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 15,
 
-		//Design
-        controlArrows: true,
-        verticalCentered: true,
-        resize : false,
-        paddingTop: '64px',
-        paddingBottom: '20px',
-        responsiveWidth: '100px',
-        responsiveHeight: '100px',
-	});
+    //leaflet
+    var map = L.map('mapLeaflet').setView([48.3921,-4.4769], 13);
 
-        $("#preloader").hide();
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
 
-	//leaflet
-	var map = L.map('mapLeaflet').setView([48.3921,-4.4769], 13);
+    $.getJSON("js/pano.json", function(json) {
+        for (i = 0; i < json.pano.length; i++) {
+            L.marker(json.pano[i].pos)
+                .addTo(map)
+                .bindPopup("<h1>"+json.pano[i].where+"</h1><br>"+json.pano[i].desc);
+        }
+    });
 
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
-
-	$.getJSON("js/pano.json", function(json) {
-		for (i = 0; i < json.pano.length; i++) {
-			L.marker(json.pano[i].pos)
-				.addTo(map)
-				.bindPopup("<h1>"+json.pano[i].where+"</h1><br>"+json.pano[i].desc);
-		}
-	});
+    $.fn.fullpage.reBuild(); //On OSM setup rebuild the structure
 
 });
